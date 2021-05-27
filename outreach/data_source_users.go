4a29dcf45b947fcac52a6cff2e63d0dadb98042f
kunalp-gohire <kunalp.gohire@gmail.com>
2021-05-27 10:03:12 +0530

    Removed comments

--- outreach/data_source_users.go
+++ outreach/data_source_users.go
@@ -3,10 +3,9 @@ package outreach
 import (
 	"context"
 	"fmt"
-	"terraform-provider-outreach/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	
+	"terraform-provider-outreach/client"
 )
 
 func dataSourceContact() *schema.Resource {
@@ -16,12 +15,6 @@ func dataSourceContact() *schema.Resource {
 				Type:     schema.TypeInt,
 				Optional: true,
 			},
-
-			// "attributes": {
-			// 	Type:     schema.TypeSet,
-			// 	Computed: true,
-			// 	Elem: &schema.Resource{
-			// 		Schema: map[string]*schema.Schema{
 			"email": {
 				Type:     schema.TypeString,
 				Required: true,
@@ -34,29 +27,15 @@ func dataSourceContact() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			// "createdat":{
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
+
 			"locked": {
-				Type:   schema.TypeBool,
+				Type:     schema.TypeBool,
 				Computed: true,
 			},
 			"username": {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			// "title": {
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// "updatedat":{
-			// 	Type:     schema.TypeString,
-			// 	Computed: true,
-			// },
-			// },
-			// },
-			// },
 		},
 		ReadContext: dataSourceUserRead,
 	}
@@ -65,35 +44,21 @@ func dataSourceContact() *schema.Resource {
 func dataSourceUserRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	c := m.(*client.Client)
-	email:= d.Get("email")
-    useremail:=fmt.Sprintf("%v" ,email)
+	email := d.Get("email")
+	useremail := fmt.Sprintf("%v", email)
 	user, err := c.GetDataSourceUser(useremail)
 	if err != nil {
 		return diag.FromErr(err)
 	}
-	
-	// uis := make([]interface{}, 1)
-	// ui := make(map[string]interface{})
-	// ui["email"] = user.Data.Attributes.Email
-	// ui["first_name"] = user.Data.Attributes.FirstName
-	// ui["last_name"] = user.Data.Attributes.LastName
-	// // ui["createdat"]=user.Data.Attributes.CreateAt
-	// ui["locked"] = user.Data.Attributes.Locked
-	// ui["username"] = user.Data.Attributes.UserName
-	// ui["title"] = user.Data.Attributes.Title
-	// // ui["updatedat"]=user.Data.Attributes.UpdatedAt
-	// uis[0] = ui
-	// d.Set("attributes", uis)
 
-	d.Set("email",user.Attributes.Email)
-	d.Set("firstname",user.Attributes.FirstName)
-	d.Set("lastname",user.Attributes.LastName)
-	d.Set("locked",user.Attributes.Locked)
-	d.Set("username",user.Attributes.UserName)
-	d.Set("id",user.ID)
-	// d.Set("title",user.Data.Attributes.Title)
+	d.Set("email", user.Attributes.Email)
+	d.Set("firstname", user.Attributes.FirstName)
+	d.Set("lastname", user.Attributes.LastName)
+	d.Set("locked", user.Attributes.Locked)
+	d.Set("username", user.Attributes.UserName)
+	d.Set("id", user.ID)
 	UserId := user.ID
-	uid:=fmt.Sprintf("%v" ,UserId)
+	uid := fmt.Sprintf("%v", UserId)
 	d.SetId(uid)
 	return diags
 }
